07e9685f17c541ed8e716bf2fc9ef8e57327d0d5
Will Melbourne <willmelbourne@gmail.com>
2016-03-07 17:28:59 +0000

    baby names regex search in html page

---
+++ babyNames.go
@@ -0,0 +1,39 @@
+package main
+
+import(
+    "fmt"
+    "io/ioutil"
+    "log"
+    "regexp"
+)
+
+func GetNames(filename string) {
+   results, err := ioutil.ReadFile(filename)
+   
+   if (err != nil) {
+       log.Fatal()
+   }
+   
+   regYear := regexp.MustCompile(`(Popularity in)\s([0-9]{4})(</h)`)
+   years := regYear.FindSubmatch(results)
+   
+   for i, year := range(years) {
+    fmt.Printf("%d %s\n", i, year)
+   }
+   
+   fmt.Printf("years size %d", len(years))
+   
+   regBabies := regexp.MustCompile(`(<tr align="right"><td>)(\d{1,6})(</td><td>)(\w+)(</td><td>)(\w+)(</td>)`)
+   babies := regBabies.FindAllSubmatch(results, -1)
+   
+   for i, baby := range(babies) {
+       for j, babyWords := range(baby) {
+            fmt.Printf("%d %d %s\n", i, j, babyWords)
+       }
+   }
+   
+}
+
+func TestGetNames() {
+    GetNames("./baby_names_data/baby1990.html")
+}
