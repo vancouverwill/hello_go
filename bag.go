4367026a9cd32a6fe859b7f4cd092c7569d5e97e
Will Melbourne <willmelbourne@gmail.com>
2016-03-09 14:01:48 +0000

    created and tested bag

---
+++ bag.go
@@ -0,0 +1,62 @@
+package main
+
+
+type Bag struct {
+    N int
+    First *node
+}
+
+type node struct {
+    item interface{}
+    next *node
+}
+
+func NewBag() *Bag {
+    newbag := new(Bag)
+    newbag.First = nil
+    newbag.N = 0
+    return newbag
+}
+
+func (b *Bag) Add(item interface{}) {
+    oldFirst := b.First
+    b.First = new(node)
+    b.First.item = item
+    b.First.next = oldFirst
+    b.N++
+}
+
+func (b *Bag) Remove() interface{} {
+    oldFirst := b.First
+    b.First = oldFirst
+    b.N--
+    return oldFirst.item
+}
+
+func (b *Bag) Size() int {
+    return b.N
+}
+
+func (b *Bag) IsEmpty() bool {
+    return b.First == nil
+}
+
+/*
+Iter returns items in no particular order
+*/
+func (b *Bag) Iter() <-chan interface{} {
+    ch := make(chan interface{});
+    go func () {
+        current := b.First
+        for current.next != nil {
+            ch <- current.item
+            current = current.next
+        }
+        
+        ch <- current.item
+        current = current.next
+        close(ch)
+    } ();
+    return ch
+}
+
---
+++ bag_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateEmptyBag(t *testing.T) {    
+    b := NewBag()
+    
+	assert.True(t, b.Size() == 0, "test new bag has zero objects inside")
+    b.Add(1)
+    b.Add(2)
+    assert.Equal(t, 2, b.Size(), "test new bag has zero objects inside")
+}
+
+
+
+func TestBagIteratesThroughAllItems(t *testing.T) {
+     b := NewBag()
+     
+     type bagObject struct {
+         Name string
+         inBag bool
+     }
+     
+    objectsToGoInBag :=[]bagObject{bagObject{"will", false, }, bagObject{"james", false}, bagObject{"mike", false}}
+     
+     
+    for _, bagObject := range(objectsToGoInBag) {
+        b.Add(bagObject)
+    }
+    
+    count := 0
+    for item := range b.Iter() {
+        t.Log("range")
+        t.Log(item)
+        count++
+    }
+    
+      assert.Equal(t, 3, count, "test new bag has three objects inside")
+}
+     
+func TestBagRemovesItems(t *testing.T) {
+    b := NewBag()
+    
+	assert.True(t, b.Size() == 0, "test new bag has zero objects inside")
+    b.Add(1)
+    b.Add(2)
+    assert.True(t, b.Size() == 2, "test bag with two additions has two objects inside")
+    
+    b.Remove()
+    
+    assert.Equal(t, 1, b.Size(), "test bag with two additions and one removed has one objects inside")
+}
