220a9fa47cdd446974e51539735dea030f23895c
Will Melbourne <willmelbourne@gmail.com>
2016-03-09 14:05:37 +0000

    created and tested rotate array in place algo

---
+++ rotateArrayInPlace.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+RotateArrayInPlace i.e. 0,1,2,3,4  -> 3,4,0,1,2
+
+reverse
+then reverse back two halves around end of new array
+i.e. if moving two indexs forward out of a array of size size then reverse from 0->1 then 2->5
+
+has no return value because slices passed as arguments reference the original data
+*/
+func RotateArrayInPlace(a []int, rotateAmount int) {
+    rotateAmount = rotateAmount % len(a)
+    if rotateAmount == 0 { return }
+    
+    if rotateAmount < 0 { rotateAmount = len(a) + rotateAmount }
+    
+    reverseArraySection(a, 0, len(a) - 1)
+    reverseArraySection(a, 0, rotateAmount - 1)
+    reverseArraySection(a, rotateAmount, len(a) - 1)
+}
+
+/*
+reverseArraySection has no return value because slices passed as arguments reference the original data
+*/
+func reverseArraySection(a []int, lo int, hi int)  {
+    for lo < hi {
+        a[lo], a[hi] = a[hi], a[lo]
+        hi--
+        lo++
+    }
+}
---
+++ rotateArrayInPlace_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestReverseArraySection(t *testing.T) {
+    input := []int{0,1,2,3,4}
+    
+    reverseArraySection(input, 1,2)
+    expected := []int{0,2,1,3,4}
+    
+    assert.Equal(t, expected, input, "successful")
+}
+
+func TestReverseWholeArray(t *testing.T) {
+    input := []int{0,1,2,3,4}
+    
+    reverseArraySection(input, 0,len(input)-1)
+    expected := []int{4,3,2,1,0}
+    
+    assert.Equal(t, expected, input, "successful")
+}
+
+func TestRotateArray(t *testing.T) {
+    input := []int{0,1,2,3,4}
+    
+    RotateArrayInPlace(input, 2)
+    expected := []int{3,4,0,1,2}
+    
+    assert.Equal(t, expected, input, "successful")
+}
+
+func TestRotateArrayNegativeAmount(t *testing.T) {
+    input := []int{0,1,2,3,4}
+    
+    RotateArrayInPlace(input, -2)
+    expected := []int{2,3,4,0,1}
+    
+    assert.Equal(t, expected, input, "successful")
+}
