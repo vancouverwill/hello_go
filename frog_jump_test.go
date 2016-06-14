c89ae0eb9e16885721892d2189b3c5a3f53a85d6
Will Melbourne <willmelbourne@gmail.com>
2016-06-14 06:20:07 +0100

    working on linear probing hashmap

--- frog_jump_test.go
+++ frog_jump_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestFrogJump(t *testing.T) {
-	got := solution(10, 85, 30)
+	got := Solution(10, 85, 30)
 	expected := 3
 	if got != expected {
 		t.Errorf("got %d but expected %d", got, expected)
--- not_done_odd_occurences_in_array.go
+++ not_done_odd_occurences_in_array.go
@@ -4,11 +4,11 @@ package main
 A non-empty zero-indexed array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
 **/
 
-/*
+/**
 planned steps:
 - create a simple hashmap from an array the same size as the input
 - as all the input apart from one will be pairs hash map the same size as input should not have too many collisions
-- use open addressing for collision handling
+- use open addressing (also called linear probing) for collision handling
 - loop through input once for each value find in hashmap then increment one
 - loop through hashmap and find the one value which has a depth of one
 
@@ -19,10 +19,11 @@ func findOddOccurencesInArray(input []int) int {
 	// }
 	return -1
 }
-*/
+**/
 
-type hashMap struct {
+type linearProinghHashMap struct {
 	size    int
+	N       int
 	content []bucket
 }
 
@@ -30,6 +31,34 @@ type bucket struct {
 	value int
 }
 
-func (h *hashMap) insertIntoHashMap(i int) {
-	
+func NewLPHashMap(size int) *linearProinghHashMap {
+	h := new(linearProinghHashMap)
+	h.size = size
+	h.N = 0
+	h.content = make([]bucket, size)
+	return h
+}
+
+func (h *linearProinghHashMap) insertIntoLPHashMap(i int) {
+	k := h.hash(i)
+	for {
+		if h.content[k].value == 0 {
+			break
+		}
+		k++
+	}
+	h.content[k] = bucket{i}
+	h.N++
+}
+
+func (h linearProinghHashMap) getSize() int {
+	return h.size
+}
+
+func (h linearProinghHashMap) getN() int {
+	return h.N
+}
+
+func (h linearProinghHashMap) hash(i int) int {
+	return i % h.size
 }
